3309c8823148dfc0c1408879e1d4d0cad4489254
Ashley <arar3302@gmail.com>
2021-08-03 20:28:29 -0400

    Initial Commit

---
+++ Codepath Project/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Codepath Project
+//
+//  Created by Ashley R on 8/3/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
